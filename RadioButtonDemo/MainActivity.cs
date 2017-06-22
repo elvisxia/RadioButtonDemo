a2ba03ac8b44a24f2467ca5a6cefa29a21044583
Elvis Xia (Shang Hai Wei Chuang Ruan Jian) <v-xucxia@microsoft.com>
2017-06-22 08:48:40 +0800

    Add project files.

---
+++ RadioButtonDemo/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace RadioButtonDemo
+{
+    [Activity(Label = "RadioButtonDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        RadioGroup radioGroup;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            radioGroup = FindViewById<RadioGroup>(Resource.Id.mGroup);
+
+            for (int i = 0; i < radioGroup.ChildCount; i++)
+            {
+                var child=radioGroup.GetChildAt(i);
+                if (child is RadioButton)
+                {
+                    ((RadioButton)child).FocusChange += RadioButton_FocusChange;
+                }
+            }
+        }
+        
+
+        private void RadioButton_FocusChange(object sender, Android.Views.View.FocusChangeEventArgs e)
+        {
+            if (e.HasFocus){
+                ((RadioButton)sender).Checked = true;
+            }
+        }
+    }
+}
+
